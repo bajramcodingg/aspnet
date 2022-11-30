1221feef357e5e7aa2b271c0a34b033b9a6c7519
Bayram <bayrambaxwiyev@gmail.com>
2022-11-30 16:32:26 +0400

    can u believe

---
+++ Developer/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Developer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace Developer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Info(string Name,string Email,string Subject,string Message)
+        {
+            User user = new User()
+            { Name=Name,
+            Email=Email,
+            Message=Message,
+            Subject=Subject
+            
+            };
+
+            return Content(user.Name+" "+user.Email+" "+user.Subject+" "+user.Message);
+        }
+    }
+}
